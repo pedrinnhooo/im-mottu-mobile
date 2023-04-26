73993769b3a89cbdfb2aac1bbc569a6c9f50fbbf
pedrinnhooo <pedroruffo0305@gmail.com>
2023-04-26 16:10:51 -0300

    level-1

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -29,24 +29,103 @@ class _MarvelScreenState extends State<MarvelScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Personagens da Marvel'),
-        centerTitle: true,
-      ),
-      body: ListView.builder(
-        itemCount: _data.length,
-        itemBuilder: (BuildContext context, int index) {
-          final character = _data[index];
-          return ListTile(
-            leading: Image.network(character['thumbnail']['path'] +
-                '.' +
-                character['thumbnail']['extension']),
-            title: Text(character['name']),
-            subtitle: Text(character['description']),
-          );
-        },
-      ),
-    );
+        backgroundColor: Colors.black87,
+        appBar: AppBar(
+          backgroundColor: Colors.red.shade900,
+          title: const Text(
+            'Personagens da Marvel',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 14,
+              fontWeight: FontWeight.bold,
+            ),
+          ),
+          centerTitle: true,
+        ),
+        body: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          color: Colors.transparent,
+          child: GridView.builder(
+              gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+                maxCrossAxisExtent: 230,
+                mainAxisExtent: 210,
+                crossAxisSpacing: 2,
+              ),
+              itemCount: _data.length,
+              itemBuilder: (BuildContext context, int index) {
+                final character = _data[index];
+                return Padding(
+                  padding: const EdgeInsets.all(16),
+                  child: SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: InkWell(
+                        onTap: () {},
+                        child: Container(
+                          margin: const EdgeInsets.only(
+                              left: 10, top: 20, right: 10, bottom: 10),
+                          height: 200,
+                          width: double.infinity,
+                          decoration: BoxDecoration(
+                            gradient: LinearGradient(
+                              begin: Alignment.topRight,
+                              end: Alignment.bottomLeft,
+                              colors: [
+                                Colors.black87,
+                                Colors.red.shade900,
+                              ],
+                            ),
+                            borderRadius: BorderRadius.circular(20),
+                            boxShadow: const [
+                              BoxShadow(
+                                color: Colors.black38,
+                                blurRadius: 10,
+                                spreadRadius: 2,
+                                offset: Offset(2, 2), //
+                              ),
+                            ],
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Padding(
+                                padding: const EdgeInsets.only(top: 12),
+                                child: ClipOval(
+                                  child: Material(
+                                    child: Image.network(
+                                        character['thumbnail']['path'] +
+                                            '.' +
+                                            character['thumbnail']['extension'],
+                                        width: 95,
+                                        height: 80,
+                                        fit: BoxFit.cover),
+                                  ),
+                                ),
+                              ),
+                              SizedBox(
+                                width: 150,
+                                child: Padding(
+                                  padding:
+                                      const EdgeInsets.only(top: 14, left: 10),
+                                  child: Center(
+                                    child: Text(
+                                      character['name'],
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 16,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      )),
+                );
+              }),
+        ));
   }
 
   Future<void> _getData() async {
